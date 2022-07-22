96f1bb9ba233d19def42192013fcd2c3a164cce0
jesus_isao <jesusisao@gmail.com>
2022-07-22 14:22:29 +0900

    fix custom-ruby

--- .zprezto-contrib/custom-ruby/init.zsh
+++ .zprezto-contrib/custom-ruby/init.zsh
@@ -2,14 +2,21 @@
 if (( ! $+commands[ruby] )); then
   return 1
 fi
+if (( ! $+commands[rbenv] )); then
+  return 1
+fi
 
 # for Ruby
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
+
+if (( ! $+commands[brew] )); then
+  return 1
+fi
+
 local OPENSSL_PATH=$(brew --prefix openssl)
 export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$OPENSSL_PATH"
 
-# Return if requirements are not found.
 if (( ! $+commands[bundle] )); then
   return 1
 fi
