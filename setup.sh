f86a704cda9444e0583fab8c512b7d1ebd9951e6
jesus_isao <jesusisao@gmail.com>
2024-06-16 20:42:02 +0900

    fix setup

--- setup.sh
+++ setup.sh
@@ -33,13 +33,13 @@ defaults write -g KeyRepeat -int 1
 
 # VSCodeの設定 --------
 
-VSCODE_SETTING_DIR="~/Library/Application\ Support/Code/User"
+VSCODE_SETTING_DIR="${HOME}/Library/Application Support/Code/User"
 CODE_COMMAND_DIR="/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
 
-rm "$VSCODE_SETTING_DIR/settings.json"
-ln -s "vscode/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
+rm -f "${VSCODE_SETTING_DIR}/settings.json"
+ln -s "${PWD}vscode/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
 
 cat vscode/extensions | while read line
 do
-    "$CODE_COMMAND_DIR/code" --install-extension $line
+    "${CODE_COMMAND_DIR}/code" --install-extension $line
 done
