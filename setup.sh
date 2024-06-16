244df994bd6fa0f0202c32ab72c62a698fa02557
jesus_isao <jesusisao@gmail.com>
2024-06-16 21:01:58 +0900

    fix setup

--- setup.sh
+++ setup.sh
@@ -17,13 +17,8 @@ for f in .??*; do
     ln -snfv ${PWD}/"$f" ~/
 done
 
-# homebrewの設定とアプリケーションのインストール --------
+# アプリケーションのインストール --------
 
-if ! type brew &> /dev/null ; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-else
-    echo "Since Homebrew is already installed, skip this phase and proceed."
-fi
 brew bundle install --file=Brewfile
 
 # キーリピート系の速度変更 --------
