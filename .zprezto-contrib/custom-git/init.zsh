a95210bfdd9df18f635f256353c7dd9f3439457f
Isaya Yokoyama <isaya.yokoyama@smarthr.co.jp>
2025-07-14 16:35:47 +0900

    optimize for a new company

--- .zprezto-contrib/custom-git/init.zsh
+++ .zprezto-contrib/custom-git/init.zsh
@@ -3,21 +3,6 @@ if (( ! $+commands[git] )); then
   return 1
 fi
 
-git-tempscript () {
-  point1=$1
-  if [ -z "$point1" ]; then
-    point1="origin/master"
-  fi
-
-  point2=$2
-  if [ -z "$point2" ]; then
-    point2="origin/develop"
-  fi
-  git fetch
-  echo -n -e "\n"
-  git --no-pager diff $point1..$point2 --name-only -- "temp_scripts/"
-}
-
 git-db-migrate () {
   point1=$1
   if [ -z "$point1" ]; then
@@ -49,18 +34,39 @@ git-tempscript-summary () {
 }
 
 git-release-list () {
+  git fetch --prune-tags
+  LATEST_TAG=`git describe --tags --abbrev=0`
+  echo -n -e "\n"
+
   point1=$1
   if [ -z "$point1" ]; then
-    point1="origin/master"
+    point1=$LATEST_TAG
   fi
 
   point2=$2
   if [ -z "$point2" ]; then
-    point2="origin/develop"
+    point2="origin/main"
   fi
-  git fetch
-  echo -n -e "\n"
+
   git --no-pager log --no-merges --no-decorate --oneline --left-right $point1...$point2
+}
+
+git-release-diff () {
+  git fetch --prune-tags
+  LATEST_TAG=`git describe --tags --abbrev=0`
+  echo -n -e "\n"
+
+  point1=$1
+  if [ -z "$point1" ]; then
+    point1=$LATEST_TAG
+  fi
+
+  point2=$2
+  if [ -z "$point2" ]; then
+    point2=origin/main
+  fi
+
+  git --no-pager diff --name-only $point1..$point2
 }
 
 git-release-list-auth () {
